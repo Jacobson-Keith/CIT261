dc8c260a7ba32bfd122a265676f3019a4e0afb18
Jacobson-Keith <ke.i.s.c.jacobson@gmail.com>
2018-12-05 22:58:46 -0800

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,90 +1,25 @@
-//<!-- Examine the Document Object Models-->
+var itemCounter = 5;
+var btn = document.getElementById("btn");
 
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// //document.title =  123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[10]);
-// // document.all[10].textContent = 'Hello';
-//console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
+btn.addEventListener("click", function(){
+  // createElement
 
-//<!-- GetElementById -->
-// console.log(document.getElementById('header-title'));
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = 'Goodbye';
-// console.log(headerTitle.innerText);
-// headerTitle.innerHTML = '<h3>Hello</h3>';
-// header.style.borderBottom = 'solid 3px #000';
+  //Create a list
+  var newLi = document.createElement('LI');
 
-//<!-- getElementsByClassName -->
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello everybody';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'blue';
+  //Create a text node
+  var newLiText = document.createTextNode('Item ' + itemCounter);
 
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
+  //add class so it looks like the other listed lists
+  newLi.className = 'list-group-item';
 
-// for(var i = 0; i < items.length; i++){
-//   items[i].style.backgroundColor = '#f4f4f4';
-// }
+  //append text to list
+  newLi.appendChild(newLiText);
 
-//<!-- getElementsByTagName -->
-//var li = document.getElementsByTagName('li');
-//console.log(li);
-//console.log(li[1]);
-//li[1].textContent = 'Hello 2';
-//li[1].style.fontWeight = 'bold';
-//li[1].style.backgroundColor = 'yellow';
+  //tell it where to add new Item Element
+  var items = document.querySelector('body .list-group');
+  var li = document.querySelector('body li');
 
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
-
-// for(var i = 0; i < li.length; i++){
-//   li[i].style.backgroundColor = '#f4f4f4';
-// }
-
-//<!-- querySelector -->
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom = 'solid 4px #ccc';
-
- var input = document.querySelector('input');
- input.value = 'Hello World'
-
-// var submit = document.querySelector('input[type="submit"]');
-// submit.value="SEND"
-
-// var item = document.querySelector('.list-group-item');
-// item.style.color = 'red';
-
-// var lastItem = document.querySelector('.list-group-item:last-child');
-// lastItem.style.color = 'blue';
-
- var secondItem = document.querySelector('.list-group-item:nth-child(2)');
- secondItem.style.color = 'coral';
-
-//<!-- querySelectorAll -->
-// var titles = document.querySelectorAll('.title');
-
-// console.log(titles);
-// titles[0].textContent = 'Hello';
-
-// var odd = document.querySelectorAll('li:nth-child(odd)');
-// var even= document.querySelectorAll('li:nth-child(even)');
-
-// for(var i = 0; i < odd.length; i++){
-//   odd[i].style.backgroundColor = '#f4f4f4';
-//   even[i].style.backgroundColor = '#ccc';
-// }
+  items.insertBefore(newLi, li);
+  itemCounter++;
+})
