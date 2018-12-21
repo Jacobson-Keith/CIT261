1cdbfd6fc3e3fc97bbbcc470c5cb7e1fb83a8ba7
Jacobson-Keith <ke.i.s.c.jacobson@gmail.com>
2018-12-20 20:57:41 -0800

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -16,7 +16,9 @@ function renderHTML(data) {
 
 // scroll through data to show all the arrays
   for (i = 0; i < data.length; i++) {
-    htmlString += "<p>" + data[i].name + " lives in " + data[i].address.city + ".</p>"
+    htmlString += "<p>" + data[i].name + " lives in " + data[i].address.city + ", if care to know the geolocation, that would be latitude: " + data[i].address.geo.lat + " and longitude: " + data[i].address.geo.lat + "." + 
+    " " + data[i].name + " works for " + data[i].company.name + " doing " + data[i].company.catchPhrase + " work." + "<br/>" + "You can also visit their website by going to: " + data[i].website + "!" +
+    " If you want to contact " + data[i].name + " you can email or call at: " + "<ul>" + data[i].email + "  OR <br/>" + data[i].phone + "</ul>" + "<hr/>" + "</p>"
   }
 
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
